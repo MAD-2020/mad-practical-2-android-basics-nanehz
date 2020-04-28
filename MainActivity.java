314e2491c798df745845198245062b62f3274fa7
nanehz <59910583+nanehz@users.noreply.github.com>
2020-04-28 17:59:42 +0800

    Whack-A-Mole
    completed.

--- MainActivity.java
+++ MainActivity.java
@@ -1,38 +1,115 @@
-package sg.edu.np.WhackAMole;
+package com.example.whack_a_mole;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.Button;
 import android.util.Log;
-
-
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    /* Hint
-        - The function setNewMole() uses the Random class to generate a random value ranged from 0 to 2.
-        - Feel free to modify the function to suit your program.
-    */
+
+    private Button leftbutton;
+    private Button middlebutton;
+    private Button rightbutton;
+    private int point = 0;
+    private TextView score;
+    private static final String TAG = "MessagePrompt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.v(TAG, "Finished Pre-Initialisation!");
+        leftbutton = (Button) findViewById(R.id.leftbutton);
+        middlebutton = (Button) findViewById(R.id.middlebutton);
+        rightbutton = (Button) findViewById(R.id.rightbutton);
+        score = findViewById(R.id.score);
+
+
     }
 
     @Override
     protected void onStart(){
         super.onStart();
         setNewMole();
-        Log.v(TAG, "Starting GUI!");
+        Log.v(TAG, "Application launched!");
     }
 
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.v(TAG, "Application launched!");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.v(TAG, "Application launched!");
+    }
 
     public void setNewMole()
     {
-        Random ran = new Random();
-        int randomLocation = ran.nextInt(3);
+        Random rand = new Random();
+        int n = rand.nextInt(3);
+
+        Button[] ButtonArray = {leftbutton,middlebutton,rightbutton};
+        Button mole = ButtonArray[n];
+
+        for ( Button b : ButtonArray)
+        {
+            if (b== mole)
+            {
+                b.setText("*");
+            }
+            else
+            {
+                b.setText("O");
+            }
+        }
+
     }
+
+    public boolean checkMole(String s)
+    {
+        if (s == "*")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void setScore()
+    {
+
+
+        String Scores = String.valueOf(point);
+        score.setText(Scores);
+
+    }
+    public void onClickButton(View v)
+    {
+        Button button = (Button) v;
+
+        if (checkMole(button.getText().toString()))
+        {
+            point +=1;
+        }
+        else
+        {
+            point-=1;
+        }
+
+
+        setNewMole();
+        setScore();
+
+    }
+
 }
